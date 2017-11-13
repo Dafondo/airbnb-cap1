bad8205ae14eb9645c46a4c5f0c8188f4ccdf94b
Austin Wang <austin.wang0311@gmail.com>
2017-11-13 01:15:57 -0500

    Added graphs

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, g
+from flask import Flask, request, render_template, g, jsonify
 import sqlite3
 from math import pi
 
@@ -39,41 +39,12 @@ def root():
     if request.method == "GET":
         return render_template('index.html')
 
-@app.route('/charts')
-def charts():
-    if request.method == "GET":
-        return render_template('charts.html')
-
-@app.route('/tables')
-def tables():
-    if request.method == "GET":
-        return render_template('tables.html')
-
-@app.route('/test')
-def test():
-    conn = get_db().cursor()
-    lat = 37.769310377340766
-    lon = -122.43385634488999
-    results = query_db('SELECT id FROM listings WHERE latitude <= %f and longitude <= %f' % (lat, lon))
-    # results = conn.execute('SELECT id FROM listings WHERE latitude <= %f and longitude <= %f' % (lat, lon)).fetchone()[0]
-    returnVal = ''
-    for result in results:
-        returnVal += str(result['id']) + '\n'
-    return returnVal
-
 @app.route('/calc', methods=['GET', 'POST'])
 def calc():
     if request.method == 'POST':
         lat = float(request.form['lat'])
         lon = float(request.form['lon'])
         r = 0.001
-        # results = query_db('''\
-        # SELECT * FROM listings WHERE (6371 * 2 * 
-        # ATAN2(SQRT(SIN((latitude - %f)*PI/180/2) * SIN((latitude - %f)*PI/180/2) + COS(latitude)
-        #  * COS(%f) * SIN((longitude - %f)*PI/180/2) * SIN((longitude - %f)*PI/180/2)),
-        # SQRT(1 - (SIN((latitude - %f)*PI/180/2) * SIN((latitude - %f)*PI/180/2) + COS(latitude)
-        #  * COS(%f) * SIN((longitude - %f)*PI/180/2) * SIN((longitude - %f)*PI/180/2)))))
-        #  < %f''' % (lat, lat, lat, lon, lon, lat, lat, lat, lon, lon, r))
         results = query_db('SELECT * FROM listings WHERE abs(latitude - %f) < %f and abs(longitude - %f) < %f' % (lat, r, lon, r))
         if len(results) < 1:
             return render_template('calculator.html', lat=lat, lon=lon)
@@ -92,6 +63,27 @@ def calc():
         return render_template('calculator.html', lat=lat, lon=lon, weeklyEst = weeklyEst)
     else:
         return render_template('calculator.html')
+
+@app.route('/stats', methods=['GET'])
+def stats():
+    if request.method == 'GET':
+        results = []
+        for i in xrange(0, 3):
+            results.append([])
+        g1 = query_db('SELECT AVG(availability_30), AVG(review_scores_rating) FROM listings GROUP BY availability_30 ORDER BY availability_30 ASC')
+        g2 = query_db('SELECT DISTINCT neighborhood_cleansed, AVG(CAST(SUBSTR(price, 2) as FLOAT)) FROM listings GROUP BY neighborhood_cleansed ORDER BY neighborhood_cleansed ASC')
+        g3 = query_db('SELECT DISTINCT host_response_time, AVG(review_scores_communication) FROM listings GROUP BY host_response_time ORDER BY host_response_time ASC')
+        for item in g1:
+            if item[0] != 0:
+                results[0].append([item[0], round(item[1],2)])
+        for item in g2:
+            results[1].append([item[0], round(float(item[1]),2)])
+        for item in g3:
+            if item[0] != '':
+                results[2].append([item[0], round(item[1],2)])
+
+        return jsonify(results)
+        
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 80, debug = True)
--- static/js/sb-admin-charts.js
+++ static/js/sb-admin-charts.js
@@ -1,58 +1,151 @@
-$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
-  
-      Highcharts.chart('myAreaChart', {
-          chart: {
-              zoomType: 'x'
-          },
-          title: {
-              text: 'USD to EUR exchange rate over time'
-          },
-          subtitle: {
-              text: document.ontouchstart === undefined ?
-                      'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-          },
-          xAxis: {
-              type: 'datetime'
-          },
-          yAxis: {
-              title: {
-                  text: 'Exchange rate'
-              }
-          },
-          legend: {
-              enabled: false
-          },
-          plotOptions: {
-              area: {
-                  fillColor: {
-                      linearGradient: {
-                          x1: 0,
-                          y1: 0,
-                          x2: 0,
-                          y2: 1
-                      },
-                      stops: [
-                          [0, Highcharts.getOptions().colors[0]],
-                          [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                      ]
-                  },
-                  marker: {
-                      radius: 2
-                  },
-                  lineWidth: 1,
-                  states: {
-                      hover: {
-                          lineWidth: 1
-                      }
-                  },
-                  threshold: null
-              }
-          },
-  
-          series: [{
-              type: 'area',
-              name: 'USD to EUR',
-              data: data
-          }]
-      });
-  });
+$.getJSON('/stats', function (data) {
+    Highcharts.chart('graph1', {
+        chart: {
+            type: 'scatter',
+            zoomType: 'xy'
+        },
+        title: {
+            text: '30 Day Availability vs Average Review Score',
+        },
+        xAxis: {
+            title: {
+                text: 'Days Available in One Month'
+            },
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true,
+            allowDecimals: false
+        },
+        yAxis: {
+            title: {
+                text: 'Average Review Score (out of 100)'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            scatter: {
+                marker: {
+                    radius: 5,
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineColor: 'rgb(100,100,100)'
+                        }
+                    }
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<b>{point.x} days available</b><br>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">Average review score:</td>' +
+                    '<td style="padding:0">${point.y:.2f}</td></tr>'
+                }
+            }
+        },
+        series: [{
+            name: 'Availability vs Score',
+            data: data[0]
+        }]
+    });
+    cat1 = new Array();
+    series1 = new Array();
+    for (var item in data[1]) {
+        cat1.push(data[1][item][0]);
+        series1.push(data[1][item][1]);
+    }
+    Highcharts.chart('graph2', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Average Price Per Day for Each Neighborhood'
+        },
+        xAxis: {
+            categories: cat1,
+            crosshair: true,
+            title: {
+                text: 'Neighborhood'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Average Price'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px"><b>{point.key}</b></span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">Price per night:</td>' +
+                '<td style="padding:0">${point.y:.2f}</td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'series1',
+            data: series1
+        }]
+    });
+    cat2 = new Array();
+    series2 = new Array();
+    for (var item in data[2]) {
+        cat2.push(data[2][item][0]);
+        series2.push(data[2][item][1]);
+    }
+    Highcharts.chart('graph3', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Host Response Time vs Average Communication Rating'
+        },
+        xAxis: {
+            categories: cat2,
+            crosshair: true,
+            title: {
+                text: 'Host Response Time'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Average Communication Rating (out of 10)'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px"><b>{point.key}</b></span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">Rating: </td>' +
+                '<td style="padding:0">{point.y:.2f}</td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'series2',
+            data: series2
+        }]
+    });
+});
